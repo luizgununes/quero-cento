60d2540610325ae8a025c077894d6ce6b5c5cc79
Muriel Lima <muriellima2@gmail.com>
2019-11-02 00:42:24 -0300

    url testes

--- queroCentoBETestes/UnitTeste.cs
+++ queroCentoBETestes/UnitTeste.cs
@@ -14,7 +14,7 @@ namespace queroCentoBETestes
 #if DEBUG
         const string URL_API = "http://localhost/api";
 #else
-        const string URL_API = "http://localhost:70/api";
+        const string URL_API = "http://localhost:80/api";
 #endif
         const string URL_TOKEN = URL_API + "/LoginApi/";
         string token;
