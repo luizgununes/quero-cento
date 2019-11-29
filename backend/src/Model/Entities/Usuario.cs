e2dc5dc06179e6982ee9536ab442016d16361fe4
Muriel <muriel.lima@quiver.net.br>
2019-11-29 14:22:03 -0300

    login

--- backend/src/Model/Entities/Usuario.cs
+++ backend/src/Model/Entities/Usuario.cs
@@ -20,12 +20,10 @@ namespace queroCentoBE.Model.Entities
         public string Username { get; set; }
         [Required]
         public string Password { get; set; }
-        [Required]
+        [BsonDefaultValue(0)]
         public TipoUsuario Tipo { get; set; }
-        [Required]
         [RegularExpression(@"(\d{2}[\.]?\d{3}[\.]?\d{3}[\/]?\d{4}[-]?\d{2})|(\d{3}[\.]?\d{3}[\.]?\d{3}[-]?\d{2})", ErrorMessage = "O documento deve estar no formato: XXX.XXX.XXX-XX ou XX.XXX.XXX/XXXX-XX")]
         public string Documento { get; set; }
-        [Required]
         [RegularExpression(@"^[aA-zZàÀ-ùÙáÁ-úÚ\\çÇ\']+((\s[aA-zZàÀ-ùÙáÁ-úÚ\\çÇ\'\.]+)+)?$", ErrorMessage = "O nome deve estar em um formato válido")]
         public string Nome { get; set; }
         public string EndRua { get; set; }
